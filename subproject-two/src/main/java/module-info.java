3bc5c6292672a171cbb2d98e5c87622e495bceec
Jonathing <me@jonathing.me>
2025-01-17 18:36:13 -0500

    Initial commit - copy of ToolchainUtils preview

---
+++ subproject-two/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module net.minecraftforge.hashutils {
+    exports net.minecraftforge.hashutils;
+
+    requires com.google.gson;
+    requires net.minecraftforge.srgutils;
+    requires org.apache.commons.io;
+
+    requires static org.jspecify;
+}
